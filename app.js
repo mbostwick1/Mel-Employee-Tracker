258558ccf1ff7f6cfcb863bad568d4854f3a0152
Melanie Bostwick <mbostwick1@gmail.com>
2020-11-25 22:46:17 -0500

    update functions

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 const figlet = require("figlet");
+const { printTable } = require("console-table-printer");
 
 let roles;
 let departments;
 let managers;
+let employees;
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -31,6 +33,7 @@ connection.connect(function (err) {
   getRoles();
   getDepartments();
   getManagers();
+  getEmployees();
 });
 
 // function which prompts the user for what action they should take
@@ -49,7 +52,7 @@ start = () => {
       } else if (answer.action === "VIEW") {
         view();
       } else if (answer.action === "UPDATE") {
-        // update();
+        update();
       } else if (answer.action === "EXIT") {
         connection.end();
       } else {
@@ -59,28 +62,35 @@ start = () => {
 };
 
 getRoles = () => {
-  connection.query("SELECT id, title FROM role", function (err, res) {
+  connection.query("SELECT id, title FROM role", (err, res) => {
     if (err) throw err;
     roles = res;
-    // console.table("roles");
   });
 };
 
 getDepartments = () => {
-  connection.query("SELECT id, name FROM department", function (err, res) {
+  connection.query("SELECT id, name FROM department", (err, res) => {
     if (err) throw err;
     departments = res;
-    // console.log("departments");
   });
 };
 
 getManagers = () => {
   connection.query(
     "SELECT id, first_name, last_name, CONCAT_WS(' ', first_name, last_name) AS managers FROM employee",
-    function (err, res) {
+    (err, res) => {
       if (err) throw err;
       managers = res;
-      // console.table("managers");
+    }
+  );
+};
+
+getEmployees = () => {
+  connection.query(
+    "SELECT id, CONCAT_WS(' ', first_name, last_name) AS Employee_Name FROM employee",
+    (err, res) => {
+      if (err) throw err;
+      employees = res;
     }
   );
 };
@@ -128,6 +138,8 @@ addDepartment = () => {
         function (err, res) {
           if (err) throw err;
           console.log("Department: " + answer.department + " added.");
+          getDepartments();
+          start();
         }
       );
     });
@@ -172,7 +184,9 @@ addRole = () => {
         function (err, res) {
           if (err) throw err;
 
-          console.log("1 new role added: " + answer.title);
+          // console.log("1 new role added: " + answer.title);
+          getRoles();
+          start();
         }
       );
     });
@@ -247,7 +261,9 @@ addEmployee = () => {
         `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${answer.first_name}', '${answer.last_name}', ${role_id}, ${manager_id})`,
         function (err, res) {
           if (err) throw err;
-          console.log("1 new role added: " + answer.title);
+          // console.log("1 new employee added: " + answer.title);
+          getEmployees();
+          start();
         }
       );
     });
@@ -274,33 +290,194 @@ view = () => {
       } else if (answer.view === "EMPLOYEE") {
         viewEmployees();
       } else if (answer.view === "EXIT") {
-        // console.log("Exit.");
+        // console.log("Exit");
         connection.end();
       }
     });
 };
 
 viewDepartments = () => {
-  connection.query("SELECT * FROM department", function(err, res) {
+  connection.query("SELECT * FROM department", function (err, res) {
     if (err) throw err;
-    console.table(res);
+    printTable(res);
     start();
   });
 };
 
 viewRoles = () => {
-  connection.query("SELECT * FROM role", function(err, res) {
-    if (err) throw err;
-    console.table(res);
-    start();
-  });
+  connection.query(
+    "SELECT  r.id, r.title, r.salary, d.name as Department_Name FROM role AS r INNER JOIN department AS d ON r.department_id = d.id",
+    function (err, res) {
+      if (err) throw err;
+      printTable(res);
+      start();
+    }
+  );
 };
 
 viewEmployees = () => {
-  connection.query("SELECT * FROM employee", function(err, res) {
-    if (err) throw err;
-    console.table(res);
-    start();
-  });
+  connection.query(
+    'SELECT e.id, e.first_name, e.last_name, d.name AS department, r.title, r.salary, CONCAT_WS(" ", m.first_name, m.last_name) AS manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id ORDER BY e.id ASC',
+    function (err, res) {
+      if (err) throw err;
+      printTable(res);
+      start();
+    }
+  );
 };
+
 // UPDATE FUNCTIONS
+update = () => {
+  inquirer
+    .prompt([
+      {
+        name: "update",
+        type: "list",
+        message: "Select to update:",
+        choices: ["Employee Role", "Employee Manager", "EXIT"],
+      },
+    ])
+    .then((answer) => {
+      if (answer.update === "Employee Role") {
+        updateRole();
+      } else if (answer.update === "Employee Manager") {
+        updateManager();
+      } else if (answer.update === "EXIT") {
+        connection.end();
+      } else {
+        connection.end();
+      }
+    });
+};
+
+updateRole = () => {
+  let employeeOptions = [];
+
+  for (var i = 0; i < employees.length; i++) {
+    employeeOptions.push(Object(employees[i]));
+  }
+  inquirer
+    .prompt([
+      {
+        name: "updateRole",
+        type: "list",
+        message: "Select employee to update.",
+        choices: function () {
+          var choiceArray = [];
+          for (var i = 0; i < employeeOptions.length; i++) {
+            choiceArray.push(employeeOptions[i].Employee_Name);
+          }
+          return choiceArray;
+        },
+      },
+    ])
+    .then((answer) => {
+      let roleOptions = [];
+      for (i = 0; i < roles.length; i++) {
+        roleOptions.push(Object(roles[i]));
+      }
+      for (i = 0; i < employeeOptions.length; i++) {
+        if (employeeOptions[i].Employee_Name === answer.updateRole) {
+          employeeSelected = employeeOptions[i].id;
+        }
+      }
+      inquirer
+        .prompt([
+          {
+            name: "newRole",
+            type: "list",
+            message: "Select a new role:",
+            choices: function () {
+              var choiceArray = [];
+              for (var i = 0; i < roleOptions.length; i++) {
+                choiceArray.push(roleOptions[i].title);
+              }
+              return choiceArray;
+            },
+          },
+        ])
+        .then((answer) => {
+          for (i = 0; i < roleOptions.length; i++) {
+            if (answer.newRole === roleOptions[i].title) {
+              newChoice = roleOptions[i].id;
+              connection.query(
+                `UPDATE employee SET role_id = ${newChoice} WHERE id = ${employeeSelected}`
+              ),
+                (err, res) => {
+                  if (err) throw err;
+                };
+            }
+          }
+          getEmployees();
+          getRoles();
+          start();
+        });
+    });
+};
+
+updateManager = () => {
+  let employeeOptions = [];
+
+  for (var i = 0; i < employees.length; i++) {
+    employeeOptions.push(Object(employees[i]));
+  }
+  inquirer
+    .prompt([
+      {
+        name: "updateManager",
+        type: "list",
+        message: "Select employee manager to update.",
+        choices: function () {
+          var choiceArray = [];
+          for (var i = 0; i < employeeOptions.length; i++) {
+            choiceArray.push(employeeOptions[i].Employee_Name);
+          }
+          return choiceArray;
+        },
+      },
+    ])
+    .then((answer) => {
+      getEmployees();
+      getManagers();
+      let managerOptions = [];
+      for (i = 0; i < managers.length; i++) {
+        managerOptions.push(Object(managers[i]));
+      }
+      for (i = 0; i < employeeOptions.length; i++) {
+        if (employeeOptions[i].Employee_Name === answer.updateManager) {
+          employeeSelected = employeeOptions[i].id;
+        }
+      }
+      inquirer
+        .prompt([
+          {
+            name: "newManager",
+            type: "list",
+            message: "Select a new manager:",
+            choices: function () {
+              var choiceArray = [];
+              for (var i = 0; i < managerOptions.length; i++) {
+                choiceArray.push(managerOptions[i].managers);
+              }
+              return choiceArray;
+            },
+          },
+        ])
+        .then((answer) => {
+          for (i = 0; i < managerOptions.length; i++) {
+            if (answer.newManager === managerOptions[i].managers) {
+              newChoice = managerOptions[i].id;
+              connection.query(
+                `UPDATE employee SET manager_id = ${newChoice} WHERE id = ${employeeSelected}`
+              ),
+                (err, res) => {
+                  if (err) throw err;
+                };
+            }
+          }
+          getEmployees();
+          getManagers();
+          start();
+        });
+    });
+};
--- seeds.sql
+++ seeds.sql
@@ -39,3 +39,16 @@ VALUES
 
 SELECT * FROM employee;
 
+-- View all employees --
+SELECT e.id, e.first_name, e.last_name, d.name AS department, r.title, r.salary, CONCAT_WS(" ", m.first_name, m.last_name) AS manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id ORDER BY e.id ASC;
+
+-- View all roles --
+SELECT  r.id, r.title, r.salary, d.name as Department_Name FROM role AS r INNER JOIN department AS d ON r.department_id = d.id;
+
+-- Get employee --
+SELECT id, CONCAT_WS(' ', first_name, last_name) AS Employee_Name FROM employee
+
+-- Update employee --
+UPDATE employee SET role_id = 3 WHERE id = 8;
+
+
